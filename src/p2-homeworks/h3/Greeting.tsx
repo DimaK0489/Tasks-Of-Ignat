68559cea60aba3c57480bbe06157563451919aa7
Dima_Kaleinik <dim040489@gmail.com>
2021-01-26 13:57:53 +0300

    unit commit

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,26 +1,26 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
 import s from "./Greeting.module.css";
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void
+    addUser: () => void
+    error: boolean
+    totalUsers: number
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error; // need to fix with (?:)
+    const inputClass = s.error ? s.errorFalse : ""; // need to fix with (?:)
 
     return (
-        <div>
+        <div className={s.content}>
             <input value={name} onChange={setNameCallback} className={inputClass}/>
             <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+            <button className={s.button} onClick={addUser}>add</button>
+            <span className={s.totalUsers}>{totalUsers}</span>
         </div>
     );
 }
