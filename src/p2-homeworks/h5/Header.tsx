afc350c10b49987d06953f05dd0d98e471e069e0
Dima_Kaleinik <dim040489@gmail.com>
2021-03-29 11:58:07 +0300

    Task number 5 NavLink, Route and CSS

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 import {PATH} from "./Routes";
+import s from "./hw5.module.css"
 
 function Header() {
     return (
-        <div>
-            <NavLink to={PATH.PRE_JUNIOR}>Pre Junior</NavLink>
+        <div className={s.header}>
+            <NavLink to={PATH.PRE_JUNIOR} className={s.links} activeClassName={s.active}> Pre Junior </NavLink>
+            <NavLink to={PATH.JUNIOR} className={s.links} activeClassName={s.active}> Junior </NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS} className={s.links} activeClassName={s.active}> JuniorPlus </NavLink>
         </div>
     );
 }
--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -2,18 +2,28 @@ import React from "react";
 import PreJunior from "./pages/PreJunior";
 import Error404 from "./pages/Error404";
 import {Route, Redirect, Switch} from "react-router-dom"
+import {Junior} from "./pages/Junior";
+import {JuniorPlus} from "./pages/JuniorPlus";
+
+
 
 export const PATH = {
     PRE_JUNIOR: "/pre-junior",
-    // add paths
+    JUNIOR: "/junior",
+    JUNIOR_PLUS: "/junior-plus"
 }
+
 
 function Routes() {
     return (
         <div>
             <Switch>
                 <Route path={"/"} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>
+
                 <Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>
+                <Route path={PATH.JUNIOR} render={() => <Junior/>}/>
+                <Route path={PATH.JUNIOR_PLUS} render={() => <JuniorPlus/>}/>
+
                 <Route render={() => <Error404/>}/>
             </Switch>
         </div>
