f3cf46dd8b2c01fe83505a7d0fbd9a6465aa10de
Dima_Kaleinik <dim040489@gmail.com>
2021-03-29 16:18:43 +0300

    Clock

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,34 +1,35 @@
 import React, {useState} from "react";
 import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+import s from "./style.module.css"
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0);
-    const [date, setDate] = useState<Date>();
+    const [date, setDate] = useState<Date>(new Date());
     const [show, setShow] = useState<boolean>(false);
 
     const stop = () => {
-        // stop
+         clearInterval(timerId)
     }
     const start = () => {
         stop();
         const id: number = window.setInterval(() => {
-            // setDate
+             setDate(new Date())
         }, 1000);
         setTimerId(id);
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     };
     const onMouseLeave = () => {
-        // close
+         setShow(false)
     };
 
-    const stringTime = "Time"; // fix with date
-    const stringDate = "Date"; // fix with date
+    const stringTime = date.getHours() + "-" + (date.getMinutes()) + "-" + date.getSeconds()
+    const stringDate = date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear()
 
     return (
-        <div>
+        <div className={s.container}>
             <div
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
@@ -37,9 +38,7 @@ function Clock() {
             </div>
 
             {show && (
-                <div>
-                    {stringDate}
-                </div>
+                <div className={s.mySpan}> {stringDate} </div>
             )}
 
             <SuperButton onClick={start}>start</SuperButton>
