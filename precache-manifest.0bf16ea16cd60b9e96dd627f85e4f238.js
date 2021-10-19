b589ceb1909410ceaf10933c8262697187fb6942
Dima_Kaleinik <dim040489@gmail.com>
2021-10-19 17:49:41 +0300

    Updates

---
+++ precache-manifest.0bf16ea16cd60b9e96dd627f85e4f238.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8683b4e5dbbb878cbbd17d13a28bc624",
+    "url": "/task01/index.html"
+  },
+  {
+    "revision": "d9cc67181aa6d6eb1465",
+    "url": "/task01/static/css/main.273c4ac5.chunk.css"
+  },
+  {
+    "revision": "cd91113e35d8218ea3d9",
+    "url": "/task01/static/js/2.7d37b30f.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/task01/static/js/2.7d37b30f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9cc67181aa6d6eb1465",
+    "url": "/task01/static/js/main.bf0a526a.chunk.js"
+  },
+  {
+    "revision": "8fa94e1e3ccf6289e80c",
+    "url": "/task01/static/js/runtime-main.26940467.js"
+  },
+  {
+    "revision": "729c9aa3ecd65ff1098de9aaf6597ec5",
+    "url": "/task01/static/media/preloader.729c9aa3.svg"
+  }
+]);
